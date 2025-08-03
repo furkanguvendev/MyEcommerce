8b4898bd8b341367f69d6a340c83624ab121db82
furkanguvenme <furkanguven.me@gmail.com>
2025-08-03 17:06:28 +0300

    Css Changes

--- ecommerce/src/components/InfoBar.tsx
+++ ecommerce/src/components/InfoBar.tsx
@@ -10,7 +10,7 @@ export const InfoBar = () => {
           <div className='w-11/12 h-full flex flex-row justify-between items-center font-montserrat text-lg text-white'> 
             <div className='flex flex-row justify-between gap-10'>
                 <p className='flex flex-row items-center gap-1'><FontAwesomeIcon icon={faPhone}/> (225 555-0118)</p>
-                <p className='flex flex-row items-center gap-1'><FontAwesomeIcon icon={faEnvelope} /> michelle.rivera@example.com</p>
+                <p className='flex flex-row items-center gap-1'><FontAwesomeIcon icon={faEnvelope} /> furkanguven.dev@gmail.com</p>
             </div>
             <p>Follow Us and get a chance to win 80% off</p>
             <p className='flex flex-row gap-5 items-center'>Follow Us&nbsp; : <div className='flex flex-row gap-3'><FontAwesomeIcon icon={faInstagram} /> <FontAwesomeIcon icon={faYoutube} /> <FontAwesomeIcon icon={faSquareFacebook} /> <FontAwesomeIcon icon={faXTwitter} /></div></p>
